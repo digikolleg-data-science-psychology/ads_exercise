cdea79ccc708ce284c236e1034ac0a85402e7f82
lucaabel <luca.abel@fau.de>
2023-02-01 12:57:10 +0100

    test nbgrader and abc-classroom

--- course-directory/ads_excercise/__init__.py
+++ course-directory/ads_excercise/__init__.py
@@ -9,6 +9,6 @@ def conf_rel_path():
     from pathlib import Path
     import sys
 
-    parent_folder = str(Path("..").resolve())
+    parent_folder = str(Path("../..").resolve())
     if parent_folder not in sys.path:
         sys.path.append(parent_folder)
